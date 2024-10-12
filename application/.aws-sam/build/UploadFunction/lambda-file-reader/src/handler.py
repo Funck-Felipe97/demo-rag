702de2af5eb12aa6524cb8b194c1a6b2a60ac51e
Felipe Carvalho Funck <funck.felipe@hotmail.com>
2024-10-12 10:05:28 -0300

    Update aplication

---
+++ application/.aws-sam/build/UploadFunction/lambda-file-reader/src/handler.py
@@ -0,0 +1,35 @@
+import json
+import os
+import boto3
+from botocore.exceptions import ClientError
+
+s3_client = boto3.client('s3')
+
+def lambda_handler(event, context):
+    try:
+        user_id = event['requestContext']['authorizer']['claims']['sub']
+        file_name = event['queryStringParameters']['file_name']
+
+        bucket_name = os.environ['BUCKET_NAME']
+        key_name = f'private/{user_id}/{file_name}'
+
+        # Gerar URL pré-assinada para o download
+        response = s3_client.generate_presigned_url('get_object',
+                                                    Params={'Bucket': bucket_name, 'Key': key_name},
+                                                    ExpiresIn=3600)
+
+        return {
+            "statusCode": 200,
+            "body": json.dumps({
+                "download_url": response
+            }),
+            "headers": {
+                "Content-Type": "application/json"
+            }
+        }
+
+    except ClientError as e:
+        return {
+            "statusCode": 500,
+            "body": json.dumps({"error": str(e)})
+        }
